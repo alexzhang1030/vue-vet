11e0693e8bb5ae442a99e1006e9139bed3b839fb
Alex <49969959+alexzhang1030@users.noreply.github.com>
2026-07-16 16:31:38 +0800

    test(rules): add reactivity fixtures

---
+++ fixtures/rules/prefer-use-template-ref/valid/use-template-ref.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useTemplateRef } from 'vue'
+
+const input = useTemplateRef('input')
+console.log(input.value)
+</script>
+
+<template>
+  <input ref="input">
+</template>
